8bfe37bb5269fddb637b4f0543e65679744cfb9f
square-story <shasadik3@gmail.com>
2025-06-11 18:36:58 +0530

    feat: Added binary search using recursion and iltration

--- DSA/BS/index.js
+++ DSA/BS/index.js
@@ -1,5 +1,7 @@
 //using the itration method
 
+const arr = [2,3,7,3,4,7,3,674,3,74,2].sort((a,b)=>a-b)
+
 const BSUsingIteration = (sortedArr, target) => {
     let left = 0
     let right = sortedArr.length - 1
@@ -12,4 +14,15 @@ const BSUsingIteration = (sortedArr, target) => {
     return -1
 }
 
-console.log(BSUsingIteration([2, 5, 6, 7, 8, 8], 2))
+
+console.log(BSUsingIteration(arr, 2))
+
+const BSUsingRecursion = (array,target,left,right)=>{
+    if(left>right) return -1
+    let middle = Math.floor((left+right)/2)
+    if(array[middle]==target) return middle
+    if(array[middle]>target) return  BSUsingRecursion(array,target,left,middle-1)
+    else return BSUsingRecursion(array,target,middle+1,right)
+}
+
+console.log(BSUsingRecursion(arr,23,0,arr.length-1))
