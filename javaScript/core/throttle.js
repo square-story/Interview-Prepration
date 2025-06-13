059aec9a43f5ea0f80c12f937ed3cfb7a5b0e399
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2025-06-13 10:44:57 +0530

    feat: Implement throttle function with detailed documentation

---
+++ javaScript/core/throttle.js
@@ -0,0 +1,25 @@
+/**
+ * What is Throttling?
+Throttling is a technique that limits how often a function can be called in a given period of time. 
+It is useful for improving the performance and responsiveness of web pages that have event listeners that trigger heavy or expensive operations,
+ such as animations, scrolling, resizing, fetching data, etc.
+ */
+
+
+/**
+ * if you have a function that fetches some data from an API every time the user scrolls the page,
+ *  you might want to throttle it so that it only makes one request every second,
+ *  instead of making hundreds of requests as the user scrolls.
+ */
+
+function throttleFun(mainFunction, delay) {
+    let timerFlag = null; //variable keep tracking to the timer
+    return (...args) => {
+        if (!timerFlag) {//if there timerflag is null it will enter to the mainfunction
+            mainFunction(...args); // Execute the main function 
+            timerFlag = setTimeout(() => { // Set a timer to clear the timerFlag after the specified delay
+                timerFlag = null; // Clear the timerFlag to allow the main function to be executed again
+            }, delay);
+        }
+    }
+}
