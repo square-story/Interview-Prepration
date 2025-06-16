29b5ef99727c31c831e85cb0681d50f51bcad86b
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2025-06-16 16:54:12 +0530

    feat: Fix findSecondLargest function to correctly track the third largest number

--- javaScript/problems/findSecondLargest.js
+++ javaScript/problems/findSecondLargest.js
@@ -4,15 +4,19 @@ const arr = [4, 6, 2, 3, 63, 43, 546, 323, 34, 2, 563, 4]
 const findSecondLargest = arr => {
     return arr.reduce((acc, curr) => {
         if (acc.largest < curr) {
+            acc.thirdLargest = acc.secondLargest
             acc.secondLargest = acc.largest
             acc.largest = curr
         } else if (acc.secondLargest < curr && curr !== acc.largest) {
+            acc.thirdLargest = acc.secondLargest
             acc.secondLargest = curr
+        } else if (acc.thirdLargest < curr && curr !== acc.secondLargest) {
+            acc.thirdLargest = curr
         }
         return acc
-    }, { largest: -Infinity, secondLargest: -Infinity })
+    }, { largest: -Infinity, secondLargest: -Infinity, thirdLargest: -Infinity })
 }
 
-const { secondLargest } = findSecondLargest(arr)
+const { secondLargest, thirdLargest } = findSecondLargest(arr)
 
-console.log(secondLargest)
+console.log(secondLargest, thirdLargest)
