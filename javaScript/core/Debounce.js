b160927070c5e9437ac20722a1f56c26254ef628
square-story <gibmepreo@gmail.com>
2025-06-26 14:13:40 +0530

    feat: Implement debounce function for rate-limiting callback execution

---
+++ javaScript/core/Debounce.js
@@ -0,0 +1,17 @@
+function debounce(callback, delay) {
+    let timerId;
+    return function (...args) {
+        clearTimeout(timerId)
+
+        timerId = setTimeout(() => {
+            callback.apply(this, args)
+        }, delay);
+    }
+}
+
+
+const something = debounce(console.log, 300)
+
+something('something')
+something('something new')
+something('something new new')
