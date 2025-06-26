a6a774a4d44c7e42676e7d95a53924986f22c4aa
square-story <gibmepreo@gmail.com>
2025-06-26 11:54:15 +0530

    feat: Enhance myFilter method to support initial value and type checking for callback

--- javaScript/core/filterPolyfill.js
+++ javaScript/core/filterPolyfill.js
@@ -2,13 +2,28 @@ let arr = require('../problems/randomArray')
 
 console.log('the Orginal Array:', arr)
 
-Array.prototype.myFilter = function (callback) {
-    let result = []
-    for (let i = 0; i < this.length; i++) {
-        if (callback(this[i], i, this)) result.push(this[i])
+Array.prototype.myFilter = function (callback, intialValue) {
+    if (typeof callback !== 'function') throw new TypeError(callback + "is not function")
+    const arr = this
+    const length = arr.length
+    const result = []
+    for (let i = 0; i < length; i++) {
+        if (i in arr) {
+            const value = arr[i]
+            if (callback.call(intialValue, value, i, arr)) {
+                result.push(value)
+            }
+        }
     }
+
     return result
 }
 
 
 console.log("The Modified Array:", arr.myFilter(item => item % 2 == 0))
+
+const something = { some: 2 }
+
+console.log("The modified with odd number", arr.myFilter(function (item) {
+    return item % this.some === 1
+}, something))
