e1daa218efb4c9893948703ebc30f88a62ed3c53
square-story <gibmepreo@gmail.com>
2025-07-01 15:18:18 +0530

    refactor: Update Fibonacci function implementation and enhance printFib for better clarity

--- DSA/Recursion/fib.js
+++ DSA/Recursion/fib.js
@@ -6,8 +6,27 @@
 
 //Write a recursive function to find the nth Fibonacci number.
 
-const Fibonacci = (n) => {
-    return n <= 1 ? n : Fibonacci(n - 1) + Fibonacci(n - 2)
+// const Fibonacci = (n) => {
+//     return n <= 1 ? n : Fibonacci(n - 1) + Fibonacci(n - 2)
+// }
+
+// console.log(Fibonacci(6))
+
+//print Fibonacci series under a limit using recursion
+
+
+
+const printFib = (limit) => {
+    let result = []
+    while (limit > 1) {
+        result.unshift(getFib(limit))
+        limit--
+    }
+    return result.join(',')
 }
 
-console.log(Fibonacci(6))
+const getFib = (n) => {
+    return n < 2 ? n : getFib(n - 1) + getFib(n - 2)
+}
+
+console.log(printFib(20))
