e00d1fac80e516b0fc126dbe11388786365f3fab
SADIK <139070345+square-story@users.noreply.github.com>
2025-07-30 13:13:58 +0530

    chors: fix some confusion around the sumOfArray problems

--- DSA/Recursion/sumOfArray.js
+++ DSA/Recursion/sumOfArray.js
@@ -2,7 +2,7 @@ const arr = [2, 6, 2, 5, 3, 56, 3]
 
 function sumOfArray(arr) {
     if (!arr.length) return 0
-    return arr[0] + sumOfArray(arr.splice(1))
+    return arr[0] + sumOfArray(arr.slice(1))
 }
 
 console.log(sumOfArray(arr))
