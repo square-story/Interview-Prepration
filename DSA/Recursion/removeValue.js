cbe296626c330551133bdc81790afe92ae1299f9
SADIK <139070345+square-story@users.noreply.github.com>
2025-07-30 13:04:09 +0530

    feat: Add removeValue and correct it removeEven

--- DSA/Recursion/removeValue.js
+++ DSA/Recursion/removeValue.js
@@ -1,20 +1,11 @@
-/**
- *  ⁠remove the even duplicate number
- */
-
-function removeEven(arr) {
-    let seen = new Map()
-    return arr.reduce((acc, curr) => {
-        if (curr % 2 == 0) {
-            if (!seen.has(curr)) {
-                seen.set(curr, 0)
-                acc.push(curr)
-            }
-        } else if (!seen.has(curr)) {
-            acc.push(curr)
-        }
-        return acc
-    }, [])
+const removeValue = (arr, value) => {
+    if (!arr.length) return []
+    let [first, ...rest] = arr
+    if (first === value) {
+        return removeValue(rest, value)
+    } else {
+        return [first, ...removeValue(rest, value)]
+    }
 }
 
-console.log(removeEven([2, 5, 2, 4, 2, 5, 3, 6, 56, 34]))
+console.log(removeValue([1, 4, 2, 4, 32, 4, 3, 2, 6], 4))
