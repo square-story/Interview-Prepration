a3e979d8fd6a77bd1ce1c177e9c7486d47b0b829
SADIK <139070345+square-story@users.noreply.github.com>
2025-08-07 11:42:46 +0530

    feat: added implemenation and explanation of unaryOperator

---
+++ javaScript/core/unaryOperator.js
@@ -0,0 +1,9 @@
+/**
+ * typeof
+ * unary plus (+ '5'  === 5)
+ * unary negation (- '5'  === -5)
+ * increment operator (5++)
+ * decrement operator (5--)
+ * logical not (!)
+ *  single operanted cases only
+ */
