f9941ea460abd6543345b0eddad02e74f24cd696
SADIK <139070345+square-story@users.noreply.github.com>
2025-09-08 16:36:19 +0530

    feat: added throttle and debounce in example things.

--- javaScript/PracticePlace/index.js
+++ javaScript/PracticePlace/index.js
@@ -1,7 +1,43 @@
 const click = document.getElementById('clickMe')
 const checkbox = document.getElementById('check')
 
+
 click.addEventListener('click', e => {
     checkbox.checked = !checkbox.checked;
     console.log(e)
 })
+
+function logClick() {
+    console.log("Throttled click at", Date.now());
+}
+
+function logInput() {
+    console.log('Debounce is triggered', Date.now().toLocaleString())
+}
+
+const searchBox = debounce(logInput, 3000)
+document.getElementById('search').addEventListener('input', searchBox)
+const throttledClick = throttle(logClick, 2000);
+document.getElementById("btn").addEventListener("click", throttledClick);
+
+function debounce(callback, delay) {
+    let timerId;
+    return function (...arg) {
+        clearTimeout(timerId)
+        timerId = setTimeout(() => {
+            callback.apply(this, arg)
+        }, delay);
+    }
+}
+
+function throttle(callback, delay) {
+    let timerId;
+    return function (...arg) {
+        if (!timerId) {
+            callback.apply(this, arg)
+            timerId = setTimeout(() => {
+                timerId = null
+            }, delay);
+        }
+    }
+}
