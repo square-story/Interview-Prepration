049bc7aad68b97845dd033f0e778f02ed0cf17a3
SADIK <gibmepreo@gmail.com>
2025-10-14 03:59:10 +0530

    Convert stdout data to string for readable output

--- nodeJS/spawn/parent.js
+++ nodeJS/spawn/parent.js
@@ -2,6 +2,6 @@ import { spawn } from 'child_process'
 
 const child = spawn('ls', ['-lh']);
 
-child.stdout.on('data', (data) => console.log(data, "yes"))
+child.stdout.on('data', (data) => console.log(data.toString(), "yes"))
 child.stderr.on('data', (data) => console.log('Error', data))
 child.on('close', () => console.log('closing the child process'))
