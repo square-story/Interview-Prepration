2b007de72d1d38ad32c5d5744dbf50f922507ca4
SADIK <gibmepreo@gmail.com>
2025-10-22 10:57:08 +0530

    Refactor non-repeating character functions for improved efficiency and readability

--- DSA/String/FindFirstNon-repeatingCharacter.js
+++ DSA/String/FindFirstNon-repeatingCharacter.js
@@ -9,4 +9,33 @@ function NonRepeating(str) {
     return [...map].filter((item) => item[1] == 1)[0][0]
 }
 
+/**
+ * 
+ * @param {string} str 
+ */
+function nonRepeatingEffecient(str) {
+    let map = {}
+    for (let el of str) {
+        map[el] = (map[el] || 0) + 1
+    }
+    for (let el of str) {
+        if (map[el] === 1) {
+            return el
+        }
+    }
+    return -1
+}
+/**
+ * 
+ * @param {string} str 
+ */
+function usingInbuildMap(str) {
+    let map = new Map()
+    for (let el of str) map.set(el, (map.get(el) || 0) + 1)
+    for (let el of str) if (map.get(el) === 1) return el
+    return -1
+}
+
 console.log(NonRepeating(str))
+console.log(nonRepeatingEffecient(str))
+console.log(usingInbuildMap(str))
