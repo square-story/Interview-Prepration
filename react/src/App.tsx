505ebacc09de58da3555fcde60ce49483859b2be
SADIK <gibmepreo@gmail.com>
2026-01-07 12:11:58 +0530

    feat: integrate Redux for state management with counter functionality
    - Added @reduxjs/toolkit and react-redux dependencies.
    - Created a counter slice with increment, decrement, and reset actions.
    - Set up Redux store and integrated it into the application.
    - Updated App component to dispatch counter actions.
    - Enhanced Navbar to display current counter value.

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,6 +1,9 @@
 
+import { useDispatch } from "react-redux"
 import "./App.css"
 import Navbar from "./components/NavBar"
+import { type AppDispatch } from "./store"
+import { decrement, increment, reset } from "./features/countSlice"
 
 
 // type IUser = {
@@ -11,12 +14,22 @@ import Navbar from "./components/NavBar"
 
 
 const App = () => {
+  const dispatch = useDispatch<AppDispatch>()
   return (
     <main className="App">
       <Navbar />
       <h1>Welcome to Themed Application</h1>
       <p>This application allows you to switch between light and dark themes.</p>
       <p>Click the button in the navbar to toggle the theme.</p>
+      <button onClick={() => dispatch(increment())}>
+        + Increment Counter
+      </button>
+      <button onClick={() => dispatch(decrement())}>
+        - Decrement Counter
+      </button>
+      <button onClick={() => dispatch(reset())}>
+        Reset Counter
+      </button>
     </main>
   )
 }
--- react/src/components/NavBar.tsx
+++ react/src/components/NavBar.tsx
@@ -1,13 +1,17 @@
+import { useSelector } from 'react-redux';
 import { useTheme } from '../hooks/useTheme.ts';
+import type { RootState } from '../store.ts';
 
 function Navbar() {
     const { theme, changeTheme } = useTheme();
+    const { value } = useSelector((state: RootState) => state.counter);
 
     return (
         <nav>
             <button onClick={changeTheme}>
                 Switch to {theme === 'light' ? 'dark' : 'light'} mode
             </button>
+            <span>Counter Value: {value}</span>
         </nav>
     );
 }
