a123c87b2253e06e1b820ec356e86bd7a7f2a5cc
SADIK <gibmepreo@gmail.com>
2026-01-18 12:44:40 +0530

    Add password hashing utilities with verification

---
+++ nodeJS/passwordHashing/index.js
@@ -0,0 +1,22 @@
+import crypto from 'crypto'
+
+function hashPassword(password) {
+    const salt = crypto.randomBytes(16).toString("hex")
+    const hash = crypto.pbkdf2Sync(password, salt, 10000, 64, "sha512").toString('hex');
+
+    return `${salt}:${hash}`
+}
+
+
+function verifyPassword(password, storedHash) {
+    const [salt, orginalHash] = storedHash.split(':')
+    const hash = crypto.pbkdf2Sync(password, salt, 10000, 64, "sha512").toString('hex');
+    return orginalHash === hash
+}
+
+const PASSWORD = "123123"
+const hashed = hashPassword(PASSWORD)
+
+console.log('Hashed', hashed)
+console.log('verify orginal', verifyPassword("123123", hashed))
+console.log("verify fake", verifyPassword("234123", hashed))
